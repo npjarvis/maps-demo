7e63d4394935e8e882bc467dc4312940e5afc71d
npjarvis <npjarvis@uw.edu>
2014-11-17 11:30:59 -0800

    Created a map pointing to uw

--- js/app.js
+++ js/app.js
@@ -6,3 +6,25 @@
 // lat: 47.655
 // lng: -122.3080
 
+
+$(document).ready(function() {
+   function createMap (center, zoom){
+       var mapElem = document.getElementById('map');
+       var map = new google.maps.Map(mapElem, {
+           center: center,
+           zoom: zoom
+       });
+
+       var marker = new google.maps.Marker({
+           position: center,
+           map: map,
+           animation: google.maps.Animation.DROP
+       });
+    }
+
+    var uwCoords = {
+        lat: 47.655,
+        lng: -122.3080
+    };
+    createMap(uwCoords, 14)
+});
